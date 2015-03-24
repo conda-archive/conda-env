f9a0b8a5b3f8fb6bf4cb5abcf6671d2746555941
Travis Swicegood <development@domain51.com>
2015-03-24 13:03:20 -0700

    Update setup.py to support unicode

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "Programming Language :: Python :: 3.3",
     ],
     description="tools for interacting with conda environments",
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst', 'rb').read().decode('UTF-8'),
     packages=[
         'conda_env',
         'conda_env.cli',
