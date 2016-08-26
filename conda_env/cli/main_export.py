d7c0d4a5f09fb0780aa9f71e59cfce1e57b35afa
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-08-26 23:37:00 +0400

    commiting fix for exporting using --path

--- conda_env/cli/main_export.py
+++ conda_env/cli/main_export.py
@@ -8,6 +8,7 @@ from conda import config
 from conda.cli import common
 
 from ..env import from_environment
+from conda.config import default_prefix
 
 description = """
 Export a given environment
@@ -47,6 +48,12 @@ def configure_parser(sub_parsers):
         help='name of environment (in %s)' % os.pathsep.join(config.envs_dirs),
         default=None,
     )
+    p.add_argument(
+        '-p', "--prefix",
+        action="store",
+        help="Full path to environment prefix (default: %s)." % default_prefix,
+        metavar='PATH',
+    )
 
     p.add_argument(
         '-f', '--file',
@@ -67,23 +74,33 @@ def configure_parser(sub_parsers):
 
 # TODO Make this aware of channels that were used to install packages
 def execute(args, parser):
+
+    if args.prefix:
+        prefix = os.path.abspath(args.prefix)
+    else:
+        prefix = common.get_prefix(args)
+
     if not args.name:
         # Note, this is a hack fofr get_prefix that assumes argparse results
         # TODO Refactor common.get_prefix
         name = os.environ.get('CONDA_DEFAULT_ENV', False)
-        if not name:
+
+        if args.prefix:
+            name = os.path.basename(prefix)
+        elif not name:
             msg = "Unable to determine environment\n\n"
             msg += textwrap.dedent("""
                 Please re-run this command with one of the following options:
 
                 * Provide an environment name via --name or -n
+                * Alternately, provide an environment path via --path or -p
                 * Re-run this command inside an activated conda environment.""").lstrip()
             # TODO Add json support
             common.error_and_exit(msg, json=False)
         args.name = name
     else:
         name = args.name
-    prefix = common.get_prefix(args)
+
     env = from_environment(name, prefix, no_builds=args.no_builds)
 
     if args.override_channels:
